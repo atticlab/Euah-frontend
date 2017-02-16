8e6d004e7fde4bcfd2b2e74abd83ad46bcfc7957
ihor <for0work0@gmail.com>
2017-02-16 17:46:29 +0200

    SMAR-599 Replace all fronts to frontend repo (info)

--- docker/npm/src/info/src/components/Navbar.js
+++ docker/npm/src/info/src/components/Navbar.js
@@ -7,30 +7,34 @@ module.exports = {
     },
 
     view: function (ctrl) {
-        return <header id="topnav">
-                <div class="topbar-main">
+        return 	<header id="topnav">
+                <div class="topbar-main m-b-20">
                     <div class="container">
-                        <div class="logo">
-                            <a href="/" class="logo">
-                                <img src={"/assets/images/logo/" + Conf.logo_src} alt=""/>
-                                <span>{Conf.tr('Ledger viewer')}</span>
-                            </a>
+                        <div class="logo m-b-20">
+                            <a href="/" class="logo"><i class="md md-equalizer"></i> <span>SmartMoney Ledger Viewer</span> </a>
                         </div>
-                        <ul class="nav navbar-nav navbar-right pull-right">
-                            <div class="menu-extras">
-                                <div class="text-right flags">
-                                    <a onclick={Conf.loc.changeLocale.bind(ctrl, 'ua')} href="#">
-                                        <img src="/assets/images/ua.png" alt="UA"/>
-                                    </a>
-                                    <a onclick={Conf.loc.changeLocale.bind(ctrl, 'en')} href="#">
-                                        <img src="/assets/images/uk.png" alt="EN"/>
-                                    </a>
-                                </div>
-                            </div>
-
+                        <ul class="nav navbar-nav navbar-right pull-right hidden-xs">
+                            <li class="dropdown">
+                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                    <span class="fa fa-language align-middle m-r-5 f-s-20"></span>
+                                    <span class="align-middle">{Conf.current_language}</span>
+                                    &nbsp;
+                                    <span class="fa fa-caret-down align-middle f-s-20"></span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-user">
+                                    <li>
+                                        <a onclick={Conf.loc.changeLocale.bind(ctrl, 'en')} href="#"><img
+                                            src="/assets/img/flags/en.png"/> English</a>
+                                        <a onclick={Conf.loc.changeLocale.bind(ctrl, 'ua')} href="#"><img
+                                            src="/assets/img/flags/ua.png"/> Українська</a>
+                                        <a onclick={Conf.loc.changeLocale.bind(ctrl, 'ru')} href="#"><img
+                                            src="/assets/img/flags/ru.png"/> Русский</a>
+                                    </li>
+                                </ul>
+                            </li>
                         </ul>
                     </div>
                 </div>
-        </header>
+            </header>
     }
 };
