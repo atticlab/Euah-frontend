422afdf209711de78149d9516111db698bca981f
ihor <for0work0@gmail.com>
2017-03-03 19:19:28 +0200

    fixes

--- docker/npm/src/wallet/src/components/AuthNavbar.js
+++ docker/npm/src/wallet/src/components/AuthNavbar.js
@@ -11,9 +11,9 @@ module.exports = {
                     <a href="/" config={m.route} class="logo"><svg class="auth-logo-img"></svg></a>
                     <div class="menu-extras">
                         <div class="text-right flags">
-                            <a onclick={Conf.loc.changeLocale.bind(ctrl, 'en')} href="#"><img src="/assets/img/uk.png" alt="UK"/></a>
-                            <a onclick={Conf.loc.changeLocale.bind(ctrl, 'ua')} href="#"><img src="/assets/img/ua.png" alt="UA"/></a>
-                            <a onclick={Conf.loc.changeLocale.bind(ctrl, 'ru')} href="#"><img src="/assets/img/ru.png" alt="RU"/></a>
+                            <a onclick={Conf.loc.changeLocale.bind(ctrl, 'en')} href="#"><img src="/assets/img/flags/en.png" alt="UK"/></a>
+                            <a onclick={Conf.loc.changeLocale.bind(ctrl, 'ua')} href="#"><img src="/assets/img/flags/ua.png" alt="UA"/></a>
+                            <a onclick={Conf.loc.changeLocale.bind(ctrl, 'ru')} href="#"><img src="/assets/img/flags/ru.png" alt="RU"/></a>
                         </div>
                     </div>
 
