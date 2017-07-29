c9fec65579222ee6763d7f64dc346f1fbf402a75
Ihor Siryi <for0work0@gmail.com>
2017-07-29 20:56:04 +0300

    sensors list fixed

--- docker/npm/src/backoffice/src/pages/Sensors/index.js
+++ docker/npm/src/backoffice/src/pages/Sensors/index.js
@@ -110,57 +110,63 @@ module.exports = {
             <div class="content-page">
                 <div class="content">
                     <div class="container">
-                        <div class="panel panel-color panel-primary">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">{Conf.tr('Sensors')}</h3>
-                            </div>
-                            <div class="panel-body">
-                                <div class="alert alert-info">
-                                    {Conf.tr('This page allows to manage sensors.')}
+                        {
+                            ctrl.sensors().length ?
+                                <div class="panel panel-color panel-primary">
+                                    <div class="panel-heading">
+                                        <h3 class="panel-title">{Conf.tr('Sensors')}</h3>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="alert alert-info">
+                                            {Conf.tr('This page allows to manage sensors.')}
+                                        </div>
+                                        <table class="table table-bordered">
+                                            <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>{Conf.tr('Account ID')}</th>
+                                                <th>{Conf.tr('Address')}</th>
+                                                <th>{Conf.tr('Comment')}</th>
+                                                <th>{Conf.tr('Status')}</th>
+                                                <th>{Conf.tr('Actions')}</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            {
+                                                ctrl.sensors().map(function(sensor, index) {
+                                                    return <tr>
+                                                        <th scope="row">{index + 1}</th>
+                                                        <td>
+                                                            <span title={sensor.account_id}>{sensor.account_id}</span>
+                                                        </td>
+                                                        <td>
+                                                            <span>{sensor.address || Conf.tr('No data yet')}</span>
+                                                        </td>
+                                                        <td>
+                                                            <span>{sensor.comment || Conf.tr('No data yet')}</span>
+                                                        </td>
+                                                        <td>
+                                                            { !sensor.address.length ?
+                                                                <button type="submit"
+                                                                        onclick={ctrl.approveSensor().bind(ctrl, sensor.account_id)}
+                                                                        class="btn btn-success btn-xs waves-effect waves-light">{Conf.tr('Approve')}</button>
+                                                                :
+                                                                ''
+                                                            }
+                                                            <button type="submit"
+                                                                    onclick={ctrl.deleteSensor.bind(ctrl, sensor.account_id)}
+                                                                    class="btn btn-danger btn-xs waves-effect waves-light">{Conf.tr('Delete')}</button>
+                                                        </td>
+                                                    </tr>
+                                                })
+                                            }
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
-                                <table class="table table-bordered">
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>{Conf.tr('Account ID')}</th>
-                                        <th>{Conf.tr('Address')}</th>
-                                        <th>{Conf.tr('Comment')}</th>
-                                        <th>{Conf.tr('Status')}</th>
-                                        <th>{Conf.tr('Actions')}</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {ctrl.sensors().map(function(sensor, index) {
-                                        return <tr>
-                                            <th scope="row">{index + 1}</th>
-                                            <td>
-                                                <span title={sensor.account_id}>{sensor.account_id}</span>
-                                            </td>
-                                            <td>
-                                                <span>{sensor.address || Conf.tr('No data yet')}</span>
-                                            </td>
-                                            <td>
-                                                <span>{sensor.comment || Conf.tr('No data yet')}</span>
-                                            </td>
-                                            <td>
-                                                { !sensor.address.length ?
-                                                    <button type="submit"
-                                                            onclick={ctrl.approveSensor().bind(ctrl, sensor.account_id)}
-                                                            class="btn btn-success btn-xs waves-effect waves-light">{Conf.tr('Approve')}</button>
-                                                    :
-                                                    ''
-                                                }
-                                                <button type="submit"
-                                                        onclick={ctrl.deleteSensor.bind(ctrl, sensor.account_id)}
-                                                        class="btn btn-danger btn-xs waves-effect waves-light">{Conf.tr('Delete')}</button>
-                                            </td>
-                                        </tr>
-                                    })}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
+                                :
+                                <p>No sensors registered yet</p>
+                        }
                     </div>
                 </div>
             </div>
